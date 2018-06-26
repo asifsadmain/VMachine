8d885ea5347a83d7e2b466e90576af422986a26b
asif2478 <sristy2478@gmail.com>
2018-06-26 13:17:45 +0600

    StateControl added

---
+++ src/statepatternex/StateControl.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statepatternex;
+
+/**
+ *
+ * @author asif
+ */
+public abstract class StateControl {
+
+    public abstract void pressSwitch(VendingMachine context, int money);
+}
